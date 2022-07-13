c8fcb383f25c902e35b88372f70689e5f1ccf4d5
Ben Tomsett <ben@tomsett.xyz>
2022-07-13 04:38:26 +0100

    try/catch missing_scope errors when checking inChannel

--- src/middleware/inChannel.ts
+++ src/middleware/inChannel.ts
@@ -1,7 +1,5 @@
 import { Middleware, SlackCommandMiddlewareArgs } from '@slack/bolt/dist/types';
-import {
-  needAddedToChannel,
-} from '../responses/authResponses';
+import { needAddedToChannel, needReauthorisation } from '../responses/authResponses';
 
 const inChannel: Middleware<SlackCommandMiddlewareArgs> = async ({
   ack,
@@ -12,12 +10,18 @@ const inChannel: Middleware<SlackCommandMiddlewareArgs> = async ({
   next,
 }) => {
   await ack();
-  const { members } = await client.conversations.members({ channel: command.channel_id });
-  if (!members || !members.includes(context.botUserId!)) {
-    console.error(`⚠️ Command invoked by ${command.user_id} (${command.user_name}) where Lunchbot was not in channel (${command.channel_name})`);
-    await needAddedToChannel(respond);
-  } else {
-    await next();
+  try {
+    const { members } = await client.conversations.members({ channel: command.channel_id });
+    if (!members || !members.includes(context.botUserId!)) {
+      console.error(`⚠️ Command invoked by ${command.user_id} (${command.user_name}) where Lunchbot was not in channel (${command.channel_name})`);
+      await needAddedToChannel(respond);
+    } else {
+      await next();
+    }
+  } catch (e: any) {
+    if (e.data.error === 'missing_scope') {
+      await needReauthorisation(respond);
+    }
   }
 };
 

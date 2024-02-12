7c4f73ac485a5d9f97420ee9b6c83a5be5ef05fa
bentomsett <ben@tomsett.xyz>
2024-02-12 12:23:57 +0000

    Ensure enough choices are given for /choose

--- src/commands/choose.ts
+++ src/commands/choose.ts
@@ -1,16 +1,23 @@
 import { Middleware, SlackCommandMiddlewareArgs } from '@slack/bolt/dist/types';
 import rollbar from '../misc/rollbar';
+import { invalidChooseParameters, invalidLunchParameters } from '../responses/commandResponses';
 
 const chooseCommandCallback: Middleware<SlackCommandMiddlewareArgs> = async ({
   command,
   ack,
   say,
+  respond,
 }) => {
   await ack();
 
   console.log(`⬇️ ${command.user_id} (${command.user_name}) invoked /choose`);
 
   const choices: string[] = command.text.split(' ');
+
+  if (choices.length < 2) {
+    return invalidChooseParameters(respond);
+  }
+
   const choice: string = choices[Math.floor(Math.random() * choices.length)];
 
   await say(
--- src/responses/commandResponses.ts
+++ src/responses/commandResponses.ts
@@ -6,3 +6,10 @@ export const invalidLunchParameters = async (respond: RespondFn) => {
     text: 'Enter a command in the format `/lunch length (minutes|hours)`, for example `/lunch 30 minutes` or `/lunch 1 hour`.\nOr, just type `/lunch` to set yourself at lunch.',
   });
 };
+
+export const invalidChooseParameters = async (respond: RespondFn) => {
+  await respond({
+    response_type: 'ephemeral',
+    text: 'Enter a command in the format `/choose options...`, for example `/choose hamburger pizza`. The options can be strings, numbers, or you can tag other users as an option. You must specify 2 or more options separated by spaces.',
+  });
+};

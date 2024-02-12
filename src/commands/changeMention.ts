7873787d834cb0ca4abb9903f6cc00b41dfcf5ec
bentomsett <ben@tomsett.xyz>
2024-02-12 18:39:16 +0000

    Fix for user group ID mention format

--- src/commands/changeMention.ts
+++ src/commands/changeMention.ts
@@ -1,5 +1,5 @@
 import { Middleware, SlackCommandMiddlewareArgs } from '@slack/bolt/dist/types';
-import { invalidChangeMentionParameters, invalidLunchParameters } from '../responses/commandResponses';
+import { invalidChangeMentionParameters } from '../responses/commandResponses';
 import { channels } from '../db';
 
 const changeMentionCommandCallback: Middleware<SlackCommandMiddlewareArgs> = async ({
@@ -9,17 +9,18 @@ const changeMentionCommandCallback: Middleware<SlackCommandMiddlewareArgs> = asy
 }) => {
   await ack();
 
-  console.log(`⬇️ ${command.user_id} (${command.user_name}) invoked /change_mention`);
+  console.log(`⬇️ ${command.user_id} (${command.user_name}) invoked /change_mention (with text: ${command.text})`);
 
   // Mention types (all valid for this command):
   // special mentions (e.g. @here) -> <!here>
   // user mentions                 -> <@U02S5G1SN8M|benji>
   // bot/app mentions              -> <@U06JQ5XF89F|lunchbot2>
   // channel mentions              -> <#C03PN47NT09|>
+  // user group mentions           -> <!subteam^S06JQ5XFA|frontend>
 
-  const mentionRegex = /<(([@!#])[A-Z0-9]+)(\|[a-z0-9]*)?>/i;
+  const mentionRegex = /<(([@!#]|(!subteam\^))[A-Z0-9]+)(\|[a-z0-9]*)?>/i;
 
-  if (command.text == '') {
+  if (command.text === '') {
     return invalidChangeMentionParameters(respond);
   }
 
@@ -38,8 +39,6 @@ const changeMentionCommandCallback: Middleware<SlackCommandMiddlewareArgs> = asy
     channelId: command.channel_id,
     teamId: command.team_id,
   };
-
-  console.log({ ...channelInfo, mention });
 
   await channels.updateOne(channelInfo, { $set: { ...channelInfo, mention } }, { upsert: true });
 
